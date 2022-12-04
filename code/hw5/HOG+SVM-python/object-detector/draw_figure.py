12af4ed1785ccc5a8b08e97b54d5c33dde3886fe
wty-yy <993660140@qq.com>
2022-12-04 15:52:55 +0800

    update CVPR5

---
+++ code/hw5/HOG+SVM-python/object-detector/draw_figure.py
@@ -0,0 +1,75 @@
+# -*- coding: UTF-8 -*-
+
+"""
+@author: wty-yy
+@software: PyCharm
+@file: draw_figure.py
+@time: 2022/9/25 14:02
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+config = {
+    "font.family": 'serif', # 衬线字体
+    "figure.figsize": (6, 6),  # 图像大小
+    "font.size": 14, # 字号大小
+    "font.serif": ['SimSun'], # 宋体
+    "mathtext.fontset": 'stix', # 渲染数学公式字体
+    'axes.unicode_minus': False # 显示负号
+}
+plt.rcParams.update(config)
+
+def draw(ax, img, title, norm, cmap=None):
+    if img.ndim == 2:
+        img = np.expand_dims(img, -1)
+    if img.min() < 0 or img.max() > 1:
+        if norm is None:
+            img = img - img.min()
+            img[img > 1] = 1
+        elif norm == 'line':
+            if img.min() == -np.inf:
+                img[img < 0] = 0
+            img = (img - img.min()) / (img.max() - img.min())
+        elif norm == 'clip':
+            img = img.clip(0, 1)
+        elif norm == 'upper' and img.mean() < 0.3:
+            mean = img.mean()
+            plt.hist(img.reshape([-1]))
+            img[img > 0.005] += 0.5 - img.mean()
+            img[img > 1] = 1
+    if cmap is None:
+        cmap = 'gray' if img.shape[2] == 1 else None
+    ax.imshow(img, cmap=cmap)
+    ax.set_title(title)
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+def draw_some(*arg, shape=None, origin=False):
+    n = len(arg)
+    if shape is None:
+        shape = [1, n]
+    wide, height = 3, 3  # 图像输出最小大小不能小于300px
+    sum_wide = 0
+    fact = 150
+    for i in range(n):
+        h, w = arg[i][0].shape[0:2]
+        height = max(height, h/fact)
+        wide = max(wide, w/fact)
+        sum_wide += w
+    figsize = (wide * shape[1], height * shape[0])
+    if origin:  # 根据原始图像大小进行绘图，只能绘制一行
+        fig = plt.figure(figsize=figsize)
+        now_wide, space, sum_fig = 0.05, 0.1 / n, 0.8
+        for i in range(n):
+            k = arg[i][0].shape[1] / sum_wide
+            ax = plt.axes([now_wide, 0.05, k * sum_fig, 0.8])
+            draw(ax, *(list(arg[i]) + (3 - len(arg[i])) * [None]))
+            now_wide += k * sum_fig + space
+    else:
+        fig, axes = plt.subplots(*shape, figsize=figsize)
+        axes = np.array(axes).reshape([-1])
+        for i in range(n):
+            draw(axes[i], *(list(arg[i]) + (3 - len(arg[i])) * [None]))
+        fig.tight_layout()
+    fig.savefig('tmp.png', dpi=300)
+    fig.show()
--- code/hw5/HOG+SVM-python/object-detector/test.py
+++ code/hw5/HOG+SVM-python/object-detector/test.py
@@ -7,13 +7,67 @@
 @time: 2022/12/2 20:12
 """
 
-import argparse as ap
 import json
 import configparser as cp
+import sys
+from pathlib import Path
+import cv2
+import joblib
+import matplotlib.pyplot as plt
+from draw_figure import *
+from skimage import feature
+from config import *
+
+def feat():
+    path = Path(r'../data/features/pos')
+    for feat_path in path.glob('*.feat'):
+        # print(feat_path)
+        fd = joblib.load(feat_path)
+        print(fd.shape)
+        break
+
+def hog_show():
+    path = Path(r'../data/dataset/PNGImages/pos/pos-0.png')
+    img = cv2.imread(str(path), cv2.IMREAD_GRAYSCALE)
+    fd, hog_img = feature.hog(img, orientations=orientations,
+                     pixels_per_cell=pixels_per_cell,
+                     cells_per_block=cells_per_block,
+                     visualize=True)
+    #plt.imshow(hog_img, cmap='gray')
+    fig = plt.figure(figsize=(12,6))
+    plt.subplot(2, 2, 1)
+    plt.imshow(img, cmap='gray')
+    plt.title('图像1')
+    plt.xticks([])
+    plt.yticks([])
+    plt.subplot(2, 2, 3)
+    plt.imshow(hog_img, cmap='gray')
+    plt.title('Hog1')
+    plt.xticks([])
+    plt.yticks([])
+
+    path = Path(r'../data/dataset/PNGImages/pos/pos-5.png')
+    img = cv2.imread(str(path), cv2.IMREAD_GRAYSCALE)
+    fd, hog_img = feature.hog(img, orientations=orientations,
+                              pixels_per_cell=pixels_per_cell,
+                              cells_per_block=cells_per_block,
+                              visualize=True)
+    #plt.imshow(hog_img, cmap='gray')
+    plt.subplot(2, 2, 2)
+    plt.imshow(img, cmap='gray')
+    plt.title('图像2')
+    plt.xticks([])
+    plt.yticks([])
+    plt.subplot(2, 2, 4)
+    plt.imshow(hog_img, cmap='gray')
+    plt.title('Hog2')
+    plt.xticks([])
+    plt.yticks([])
+    fig.tight_layout()
+    plt.savefig('hog特征提取.png', dpi=300)
+    plt.show()
+
+    print('特征向量维度', fd.shape)
 
 if __name__ == '__main__':
-    config = cp.RawConfigParser()
-    config.read(r'config.cfg')
-    string = json.loads(config.get('hog', 'min_wdw_sz'))
-    a = json.loads('[1,2,3]')
-    print(a)
+    hog_show()
